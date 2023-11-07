15fa29278fa65a6a097f0feebd6e774acdc6b491
arnav <arnav4c@gmail.com>
2023-11-08 01:05:17 +0530

    add more content

--- navBar.js
+++ navBar.js
@@ -8,6 +8,9 @@ const line3 = document.querySelector('.line3')
 const modeButton = document.querySelector('.mode')
 const modeSwitch = document.querySelector('.switch')
 
+// Remove this to reuse navbar
+const root = document.querySelector(':root')
+
 let navHamClick = false
 navHam.addEventListener('click', () => {
     if (!navHamClick) {
@@ -44,12 +47,15 @@ modeButton.addEventListener('click', () => {
         modeSwitch.style.left = '32px'
         body.style.backgroundColor = '#B4DDFF'
         body.style.color = '#000'
-        document.querySelector(':root').style.setProperty('--dullColor', '#B4DDFF10, #B4DDFF20, #B4DDFF');
-        document.querySelector(':root').style.setProperty('--titleColor', '#00000080')
-        document.querySelector(':root').style.setProperty('--stroke', '4px #000')
+
+        // remove this to reuse theme
+        root.style.setProperty('--dullColor', '#B4DDFF10, #B4DDFF20, #B4DDFF');
+        root.style.setProperty('--titleColor', '#00000080')
+        root.style.setProperty('--stroke', '4px #000')
+        root.style.setProperty('--boxBorder', '#00000050')
         document.querySelector('.mainHeading').style.color = '#fff'
         document.querySelector('.mainHeading').style.textShadow = '0px 0px 10px #fff'
-
+        document.querySelector('.paraDeco').style.backgroundColor = '#000'
 
         nightMode = false
     } else {
@@ -57,13 +63,14 @@ modeButton.addEventListener('click', () => {
         body.style.backgroundColor = '#212121'
         body.style.color = '#fff'
 
-        //It will change theme of the page
-        document.querySelector(':root').style.setProperty('--dullColor', '#21212160, #21212170, #212121');
-        document.querySelector(':root').style.setProperty('--titleColor', '#ffffff80')
-        document.querySelector(':root').style.setProperty('--stroke', '3px #fff')
+        // remove this to reuse theme
+        root.style.setProperty('--dullColor', '#21212160, #21212170, #212121');
+        root.style.setProperty('--titleColor', '#ffffff80')
+        root.style.setProperty('--stroke', '3px #fff')
+        root.style.setProperty('--boxBorder', '#ffffff50')
         document.querySelector('.mainHeading').style.color = '#000'
         document.querySelector('.mainHeading').style.textShadow = '0px 0px 5px #000'
-
+        document.querySelector('.paraDeco').style.backgroundColor = '#fff'
         
         nightMode = true
     }
